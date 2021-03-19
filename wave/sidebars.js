b24f4678bce2b94464d9211467e372760527421f
Eric Anderson <ericmand@gmail.com>
2021-03-18 22:31:40 -0600

    Initial

---
+++ wave/sidebars.js
@@ -0,0 +1,39 @@
+module.exports = {
+  docs: [
+    {
+      type: 'category',
+      label: 'Introduction',
+      items: [
+        'introduction',
+        'the-wave-model',
+        'wave-success',
+        'about-the-toolkit',
+        'how-to-use-the-toolkit',
+        'replicating-wave'
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Designing',
+      items: [
+        'introduction-principles',
+        'the-wave-way',
+        'implementation-tools',
+        'lessons-learned-tips',
+        'wave-support',
+        'additional-resources'
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Implementing',
+      items: [
+        'finding-applicants',
+        'training-delivery',
+        'job-matching',
+        'alumni-support',
+        'screening-assessment'
+      ],
+    },
+  ],
+};
